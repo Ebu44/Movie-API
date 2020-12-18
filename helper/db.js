be9dc0528773722e1d7cdf3af9455145f919fee2
Ebu44 <ebubekiryusuf44@hotmail.com>
2020-12-18 17:18:59 +0300

    Movie model and model router added

---
+++ helper/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+module.exports = () => {
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Connection Succesfull");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  mongoose.Promise = global.Promise;
+};
